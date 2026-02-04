d1e14ec37a51cb17b84af1f25dc981be388e8003
Dusty Thomson <dustythomson1@gmail.com>
2026-02-04 18:05:18 +0200

    Update intro_screen.dart

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -432,8 +432,7 @@ class _IntroScreenState extends State<IntroScreen> {
                 child: Container(
                   color: Colors.white,
                   child: Center(
-                   child: SizedBox.expand(
-                    child: Image.asset(
+                   child: Image.asset(
                       'assets/images/latest_pc.png',
                       width: 320,
                       fit: BoxFit.contain,
