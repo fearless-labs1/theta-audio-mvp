86a32e6e9cdd906827e7b72a2abd4c1014b246b4
Dusty Thomson <dustythomson1@gmail.com>
2026-02-09 10:44:07 +0200

    Gate startup video diagnostics overlay

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -445,6 +445,7 @@ class _IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
+    const bool showVideoDiagnostics = false;
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
@@ -492,6 +493,26 @@ class _IntroScreenState extends State<IntroScreen> {
               ),
             ),
 
+          if (showVideoDiagnostics)
+            Positioned(
+              top: 8,
+              left: 8,
+              right: 8,
+              child: IgnorePointer(
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.black54,
+                  child: Text(
+                    _debugOverlayText,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      height: 1.2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
         ],
       ),
     );
