889693b7f92b4ba152e31f3315ec438e1ddc67d6
Dusty Thomson <dustythomson1@gmail.com>
2026-02-09 10:19:06 +0200

    Remove debug overlay widget

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:theta_audio_mvp/app/router.dart';
@@ -493,26 +492,6 @@ class _IntroScreenState extends State<IntroScreen> {
               ),
             ),
 
-          if (kDebugMode)
-            Positioned(
-              top: 8,
-              left: 8,
-              right: 8,
-              child: IgnorePointer(
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.black54,
-                  child: Text(
-                    _debugOverlayText,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      height: 1.2,
-                    ),
-                  ),
-                ),
-              ),
-            ),
         ],
       ),
     );
