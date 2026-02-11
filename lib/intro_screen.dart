545ba55608765c58f35d8d6c960226e62089e8dc
Dusty Thomson <dustythomson1@gmail.com>
2026-02-11 15:42:36 +0200

    Re-enable intro video attempts on WSL by default

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -53,8 +53,6 @@ class _IntroScreenState extends State<IntroScreen> {
 
   static const bool _disableIntroVideo =
       bool.fromEnvironment('THETA_DISABLE_INTRO_VIDEO', defaultValue: false);
-  static const bool _forceIntroVideoOnWsl =
-      bool.fromEnvironment('THETA_FORCE_INTRO_VIDEO_ON_WSL', defaultValue: false);
 
   @override
   void initState() {
@@ -98,16 +96,12 @@ class _IntroScreenState extends State<IntroScreen> {
 
   String _fallbackReasonForDisabledVideo() {
     if (_disableIntroVideo) return 'video-disabled-flag';
-    if (PlatformEnv.isWSL && !_forceIntroVideoOnWsl) return 'wsl-auto-disabled';
     if (Platform.isLinux && !PlatformEnv.hasDisplay) return 'linux-no-display';
     return 'video-disabled';
   }
 
   bool get _shouldSkipIntroVideo {
     if (_disableIntroVideo) return true;
-    if (PlatformEnv.isWSL && !_forceIntroVideoOnWsl) {
-      return true;
-    }
     if (Platform.isLinux && !PlatformEnv.hasDisplay) {
       return true;
     }
